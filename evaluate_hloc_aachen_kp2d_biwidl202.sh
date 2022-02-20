70f64b145c0199b3ffa8195c9c40efc26fec1555
Simon Maurer <simomaur@student.ethz.ch>
2022-02-21 00:11:05 +0100

    added python scripts for evaluating KP2D and run scripts for biwidl202

---
+++ evaluate_hloc_aachen_kp2d_biwidl202.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#SBATCH  --output=/scratch-second/biwidl202/simomaur/log/hloc/aachen/%j.out
+#SBATCH  --constraint='titan_x|titan_xp|geforce_rtx_2080_ti'
+#SBATCH  --mem=40G
+#SBATCH  --cpus-per-task=4
+
+#source /scratch_net/biwidl202/simomaur/opt/conda/bin/conda shell.bash hook
+source /scratch_net/biwidl202/simomaur/opt/conda/etc/profile.d/conda.sh
+conda activate hloc
+python3 -u evaluate_hloc_aachen_kp2d.py "$@"
