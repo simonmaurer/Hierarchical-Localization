5025325b053eda9c092730bceb7fcb7de9aff9b5
simonmaurer <simonmaurer@users.noreply.github.com>
2022-02-20 01:24:34 +0100

    Update muri.py
    using class based methods of MURI

--- hloc/extractors/muri.py
+++ hloc/extractors/muri.py
@@ -3,36 +3,32 @@ from pathlib import Path
 
 from ..utils.base_model import BaseModel
 
+import tensorflow as tf
+import torch
 import larq_compute_engine as lce
 
-r2d2_path = Path(__file__).parent / "../../third_party/r2d2"
-sys.path.append(str(r2d2_path))
-from extract import load_network, NonMaxSuppression, extract_multiscale
+muri_path = Path(__file__).parent / "../../third_party/muri"
+sys.path.append(str(muri_path))
+from muri.detector import MURIDetector
 
 
 class MURI(BaseModel):
     default_conf = {
-        'model_name': 'r2d2_WASF_N16.pt',
-        'max_keypoints': 5000,
-        'scale_factor': 2**0.25,
-        'min_size': 256,
+        'model_name': 'muri',
+        'keep_topk_or_threshold': 0.7,
+        'min_size': 320,
         'max_size': 1024,
-        'min_scale': 0,
-        'max_scale': 1,
-        'reliability_threshold': 0.7,
-        'repetability_threshold': 0.7,
     }
     required_inputs = ['image']
 
     def _init(self, conf):
-        model_fn = r2d2_path / "models" / conf['model_name']
-        #self.norm_rgb = tvf.Normalize(mean=[0.485, 0.456, 0.406],
-                                      std=[0.229, 0.224, 0.225])
-        tflite_path = ""
-        lce_model = None
-        with open(tflite_path, 'rb') as file:
-            lce_model = file.read()
-        self.net = lce.Interpreter(lce_model, num_threads=4)
+        model_path = muri_path / "models" / conf['model_name']
+        self.detector = MURIDetector(str(model_path.absolute()), input_shape=(240,320,3), keep_topk_or_threshold=conf['keep_topk_or_threshold'])
+        #tflite_path = ""
+        #lce_model = None
+        #with open(tflite_path, 'rb') as file:
+        #    lce_model = file.read()
+        #self.net = lce.Interpreter(lce_model, num_threads=4)
 
     def _normalize(self, img):
         img -= 0.5
@@ -41,28 +37,23 @@ class MURI(BaseModel):
     
     def _forward(self, data):
         img = data['image']
+        img = tf.constant(img.numpy())
+        img = tf.transpose(img, (0,2,3,1))
         img = self._normalize(img)
         
-        descriptors, keypoints, scores = self.net.predict(img)
-        descriptors = descriptors.squeeze()
-        keypoints = keypoints.squeeze()
-        scores = scores.squeeze()
-        descriptors = descriptors.reshape((descriptors.shape[0]*descriptors.shape[1], descriptors.shape[2]))
-        keypoints = keypoints.reshape((keypoints.shape[0]*keypoints.shape[1], keypoints.shape[2]))
-        scores = scores.reshape((scores.shape[0]*scores.shape[1], ))
+        #descriptors, keypoints, scores = self.net.predict(img)
+        #descriptors = descriptors.squeeze()
+        #keypoints = keypoints.squeeze()
+        #scores = scores.squeeze()
+        #descriptors = descriptors.reshape((descriptors.shape[0]*descriptors.shape[1], descriptors.shape[2]))
+        #keypoints = keypoints.reshape((keypoints.shape[0]*keypoints.shape[1], keypoints.shape[2]))
+        #scores = scores.reshape((scores.shape[0]*scores.shape[1], ))
+        
+        scores, keypoints, descriptors = self.net.detectAndCompute(img)
 
-        xys, desc, scores = extract_multiscale(
-            self.net, img, self.detector,
-            scale_f=self.conf['scale_factor'],
-            min_size=self.conf['min_size'],
-            max_size=self.conf['max_size'],
-            min_scale=self.conf['min_scale'],
-            max_scale=self.conf['max_scale'],
-        )
-        idxs = scores.argsort()[-self.conf['max_keypoints'] or None:]
-        xy = xys[idxs, :2]
-        desc = desc[idxs].t()
-        scores = scores[idxs]
+        scores = torch.FloatTensor(scores)
+        keypoints = torch.FloatTensor(keypoints)
+        descriptors = torch.FloatTensor(descriptors).t()
 
         pred = {'keypoints': keypoints[None],
                 'descriptors': descriptors[None],
