1f40041c18d232f91ea8f696bc53ae7e0a531095
Simon Maurer <simomaur@biwidl202.ee.ethz.ch>
2022-02-23 17:20:53 +0100

    fix for biwidl202 cluster scripts using -m for calling python modules

--- evaluate_hloc_aachen_biwidl202.sh
+++ evaluate_hloc_aachen_biwidl202.sh
@@ -8,4 +8,4 @@
 #source /scratch_net/biwidl202/simomaur/opt/conda/bin/conda shell.bash hook
 source /scratch_net/biwidl202/simomaur/opt/conda/etc/profile.d/conda.sh
 conda activate hloc
-python3 -u hloc.pipelines.Aachen.pipeline "$@"
+python3 -m hloc.pipelines.Aachen.pipeline "$@"
--- evaluate_hloc_aachenv1.1_biwidl202_cpu.sh
+++ evaluate_hloc_aachenv1.1_biwidl202_cpu.sh
@@ -7,4 +7,4 @@
 #source /scratch_net/biwidl202/simomaur/opt/conda/bin/conda shell.bash hook
 source /scratch_net/biwidl202/simomaur/opt/conda/etc/profile.d/conda.sh
 conda activate hloc
-python3 -u hloc.pipelines.Aachen_v1_1.pipeline "$@"
+python3 -m hloc.pipelines.Aachen_v1_1.pipeline "$@"
